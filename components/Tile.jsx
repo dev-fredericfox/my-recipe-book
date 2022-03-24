f9c7f68d212fe68e5ec836e0a2a15019ecb460a2
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-24 10:18:24 +0100

    Addd Picture Tile

---
+++ components/Tile.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Tab({feed}){
+return (feed.map((el) => {
+    const link = `/post/${el.id}`;
+    return (
+      <div
+        className="h-96 relative rounded-2xl overflow-hidden mx-auto my-6"
+        key={el.id}
+      >
+        <Image
+          src={el.coverimg}
+          alt="{el.title}"
+          layout="fill"
+          objectFit="cover"
+        ></Image>
+        <Link href={link} passHref>
+          <div className="relative text-white bg-black bg-opacity-30 h-full">
+            <div className="absolute bottom-6 mx-4">
+            <p className="font-bold text-2xl">
+              {el.title}
+            </p>
+            <p>{el.category.name} Corner</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    );
+  }))
+}
