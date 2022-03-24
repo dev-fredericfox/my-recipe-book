4a88401e07bd1ef55b4646f8ed342e2a0bae856c
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-24 16:26:35 +0100

    Add Ingredients list

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -3,6 +3,10 @@ import Head from "next/head";
 import Image from "next/image";
 import { getAllPostIds } from "../../lib/getPostIds";
 import { getPost } from "../../lib/getPosts";
+import Layout from "../../components/Layout";
+import { ArrowSmLeftIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+import IngredientsList from "../../components/IngredientsList"
 
 const BlogPost: NextPage = ({ postData }) => {
   const post = postData[0];
@@ -14,17 +18,41 @@ const BlogPost: NextPage = ({ postData }) => {
         <meta name="description" content="Welcome to Freds Cooking Corner" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1 className="text-2xl">{post.title}</h1>
-        <div>
-          In: {post.category.name} Cuisine By: {post.author?.name ?? "Admin"}
-          Published: {post.createdAt}
-        </div>
-        <div>
-        <Image src={post.coverimg} layout="responsive" height="427px" width="640px" alt={post.title} />
-        </div>
-        <div>{post.content}</div>
-      </main>
+      <Layout>
+        <main>
+          <div className="flex flex-row mt-10 pb-6">
+            <div className="bg-white rounded-full">
+              <Link href="/" passHref>
+                <ArrowSmLeftIcon className="h-8 w-8 text-gray-300" />
+              </Link>
+            </div>
+            <div className="grow">
+              <h1 className="text-2xl font-bold text-center pr-10">
+                {post.title}
+              </h1>
+            </div>
+          </div>
+          <div className="rounded-2xl overflow-hidden shadow-md">
+            <Image
+              src={post.coverimg}
+              layout="responsive"
+              height="427px"
+              width="640px"
+              alt={post.title}
+            />
+          </div>
+          <div className="mt-6">
+            <p className="text-2xl font-bold">
+              Ingredients ({post.ingredients.length})
+            </p>
+            <IngredientsList ingredients={post.ingredients}/>
+            <p className="text-2xl mt-2 font-bold">
+              Directions
+            </p>
+          <div>{post.content}</div>
+          </div>
+        </main>
+      </Layout>
     </div>
   );
 };
