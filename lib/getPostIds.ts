0c7f8edd3270ab40558aaac40397967e489d15c0
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-24 10:18:43 +0100

    Fetch PostIds

--- lib/getPostIds.ts
+++ lib/getPostIds.ts
@@ -4,6 +4,6 @@ export async function getAllPostIds () {
     const posts = await prisma.post.findMany({
         where: { published: true }
     })
-    const postIds = posts.map(el => `/posts/${el.id.toString()}`)
+    const postIds = posts.map(el => `/post/${el.id.toString()}`)
     return postIds
 }
