1cc4c33b3b378def856a4752a1dfd45ac88e7df2
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-28 18:11:36 +0200

    Add login

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,36 @@
+import React from "react";
+import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
+
+const dash = <span className="mx-2">-</span>;
+
+const activeSession = (
+  <div className="text-center text-gray-500 flex flex-row">
+    <div>Post Overview</div> {dash}
+    <div><Link href="/create">New Post</Link></div> {dash}
+    <div onClick={() => signOut()}>Logout</div>
+  </div>
+);
+
+const inactiveSession = (
+  <div className="text-center text-gray-500 flex flex-row">
+    <div>About</div> {dash}
+    <div>Imprint</div> {dash}
+    <div>Data</div> {dash}
+    <div>
+      <Link href="/api/auth/signin">Login</Link>
+    </div>
+  </div>
+);
+
 export default function Footer() {
+  const { data: session, status } = useSession();
   return (
     <div>
-      <p className="text-center text-gray-500">About - Imprint - Data</p>
-      </div>
+      <p>Session status: {status}</p>
+      <div>{session && activeSession}</div>
+      <div>{!session && inactiveSession}</div>
+      {/* <div>{session}</div> */}
+    </div>
   );
 }
