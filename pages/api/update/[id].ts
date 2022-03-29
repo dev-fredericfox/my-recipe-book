bb54abed28514da3b9dfd4b10315c0064aade429
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-29 22:37:19 +0200

    Update Route + Form

---
+++ pages/api/update/[id].ts
@@ -0,0 +1,48 @@
+import { getSession } from "next-auth/react";
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
+  const postId = parseInt(req.query.id as string);
+
+  if (session) {
+    // Author has bug, but why?
+    // const author = { connect: { email: session?.user?.email } };
+    const data = Object.assign(req.body);
+    const connectCategory = { connect: { name: req.body.category } };
+    data.category = connectCategory
+    console.log(data);
+    console.log("postId");
+    console.log(postId);
+    try {
+      console.log("Trying...");
+      const result = await prisma.post.update({
+        where: { id: postId },
+        data: data,
+      });
+      console.log("result");
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).send({
+        message: "Database Error",
+      });
+    }
+  }
+
+  if (!session) {
+    res.status(403).send({
+      message: "You need to be logged in to perform this action.",
+    });
+  }
+
+  // console.log("result");
+  // console.log(session);
+}
---
+++ pages/edit/[id].tsx
@@ -0,0 +1,270 @@
+import React, { useState, useEffect } from "react";
+import { GetServerSideProps } from "next";
+import { getAllCategories } from "../../lib/getAllCategories";
+import type { NextPage } from "next";
+import Head from "next/head";
+import Title from "../../components/Title";
+import GenericGreenButton from "../../components/GenericGreenButton";
+import Layout from "../../components/Layout";
+import CategoryDropdown from "../../components/CategoryDropdown";
+import AddCategoryModal from "../../components/AddCategoryModal";
+import { Category, Post } from "../../lib/interfaces";
+import { getPost } from "../../lib/getPost";
+
+interface Params {
+  params: Id;
+}
+
+interface Id {
+  id: string;
+}
+
+export const getServerSideProps: GetServerSideProps<any> = async ({ params }:any) => {
+  const postId = parseInt(params.id);
+  try {
+    const categories = await getAllCategories();
+    const post = await getPost(postId);
+    return {
+      props: { categories, post, postId },
+    };
+  } catch (error) {
+    return {
+      props: "Error",
+    };
+  }
+};
+
+interface Props {
+  categories: Category[];
+  post: Post[];
+  postId: number
+}
+
+const EditPost: NextPage<Props> = ({ categories, post, postId }) => {
+    console.log(postId)
+  const [amountOfIngredients, setAmountOfIngredients] = useState(1);
+  const [title, setTitle] = useState(post[0].title);
+  const [coverimg, setCoverimg] = useState(post[0].coverimg);
+  const [category, setCategory] = useState(post[0].category.name);
+  const [content, setContent] = useState(post[0].content);
+  const [ingredients, setIngredients] = useState<Array<Ingredient>>(post[0].ingredients);
+  const [fetchError, setFetchError] = useState("");
+  const [fetchResult, setFetchResult] = useState("");
+  const [fetchStatus, setFetchStatus] = useState("");
+  const [categoryArray, setCategoryArray] = useState<Category[]>(categories);
+  const [addNewCategory, setAddNewCategory] = useState(false);
+
+  useEffect(() => {
+    const ingredients = post[0].ingredients;
+    const countIngredients = ingredients.length;
+    setAmountOfIngredients(countIngredients);
+  }, []);
+
+  const appendNewlyAddedCategoryToCategoryArray = (e: Category) => {
+    console.log(e);
+    setCategory(e.name);
+    const cache = [...categoryArray];
+    cache.push(e);
+    console.log(cache);
+    setCategoryArray(cache);
+  };
+
+  const submitData = async (published: boolean) => {
+    console.log("clicked submitted");
+    const body = {
+      title,
+      content,
+      coverimg,
+      category,
+      published,
+      ingredients,
+    };
+    console.log(body);
+    setFetchStatus("pending");
+    try {
+      let response = await fetch(`/api/update/${postId}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      let result = await response.json();
+      if (response.status === 403) {
+        throw Error("You need to be logged in to post a new recipe.");
+      }
+      if (response.status === 500) {
+        throw Error("Database Error");
+      }
+      if (response.status === 200) {
+        setFetchResult(result);
+        setFetchStatus("OK");
+        console.log(fetchResult);
+      } else {
+        throw Error("Something went wrong");
+      }
+    } catch (error: any) {
+      setFetchError(error.message);
+      setFetchStatus("Error");
+      console.error(error);
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log(fetchResult);
+  // }, [fetchResult]);
+
+  interface Ingredient {
+    emoji: string;
+    ingredient: string;
+    amount: string;
+    unit: string;
+  }
+
+  const removeIngredientAndPopArray = () => {
+    setAmountOfIngredients(amountOfIngredients - 1);
+    ingredients.pop();
+  };
+  const addIngredients = (
+    ingredientValues: string,
+    ingredientName: string,
+    index: number
+  ) => {
+    const idx = index - 1;
+
+    const cache: Ingredient[] = [...ingredients];
+    const ingredient: Ingredient = {
+      emoji: cache[idx]?.emoji ?? "",
+      ingredient: cache[idx]?.ingredient ?? "",
+      amount: cache[idx]?.amount ?? "",
+      unit: cache[idx]?.unit ?? "",
+    };
+    ingredient[ingredientName as keyof Ingredient] = ingredientValues;
+    cache[idx] = ingredient;
+    setIngredients([...cache]);
+  };
+
+  const ingredientsJSX = (key: number) => (
+    <div className="flex mt-2 flex-row" key={key}>
+      <div className="mr-2 w-20">
+        <input
+          onChange={(e) => addIngredients(e.target.value, "emoji", key)}
+          className="rounded-lg pl-5 w-full h-12 px-2"
+          type="text"
+          value={ingredients[key-1].emoji}
+          placeholder="Emoji"
+        />
+      </div>
+      <div className="mr-2 grow">
+        <input
+          onChange={(e) => addIngredients(e.target.value, "ingredient", key)}
+          className="rounded-lg pl-5 w-full h-12 px-2"
+          type="text"
+          value={ingredients[key-1].ingredient}
+          placeholder="Ingredient"
+        />
+      </div>
+      <div className="mr-2 w-24">
+        <input
+          onChange={(e) => addIngredients(e.target.value, "amount", key)}
+          className="rounded-lg pl-5 w-full h-12 px-2"
+          type="text"
+          value={ingredients[key-1].amount}
+          placeholder="Amount"
+        />
+      </div>
+      <div className="w-20">
+        <input
+          onChange={(e) => addIngredients(e.target.value, "unit", key)}
+          className="rounded-lg pl-5 w-full h-12 px-2"
+          type="text"
+          value={ingredients[key-1].unit}
+          placeholder="Unit"
+        />
+      </div>
+    </div>
+  );
+
+  const ingredientRow = (numberOfLines: number) => {
+    const rows = [];
+    for (let i = 1; i <= numberOfLines; i++) {
+      rows.push(ingredientsJSX(i));
+    }
+    return rows;
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Add Recipe</title>
+        <meta name="description" content="Add Recipe" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <div>
+          <div className="mt-6">
+            <Title title="New Recipe" />
+          </div>
+          <div className="mt-6">
+            <input
+              onChange={(e) => setTitle(e.target.value)}
+              className="rounded-lg pl-5 w-full h-12 px-2"
+              type="text"
+              value={title}
+              placeholder="Recipe Name"
+            />
+          </div>
+          <div className="flex flex-row mt-2">
+            <input
+              onChange={(e) => setCoverimg(e.target.value)}
+              className="rounded-lg pl-5 w-full h-12 px-2 mr-2"
+              type="text"
+              value={coverimg}
+              placeholder="Cover Image"
+            />
+            <CategoryDropdown
+              categories={categoryArray}
+              select={(e) => setCategory(e)}
+              selected={category}
+              addNewCategory={(): void => setAddNewCategory(!addNewCategory)}
+            />
+          </div>
+          {addNewCategory && (
+            <AddCategoryModal
+              closeModal={() => setAddNewCategory(false)}
+              addNewCategoryToDropdown={(e: Category) =>
+                appendNewlyAddedCategoryToCategoryArray(e)
+              }
+            />
+          )}
+          {ingredientRow(amountOfIngredients)}
+          <div className="flex flex-row">
+            <GenericGreenButton
+              text="Add Ingredient"
+              click={() => setAmountOfIngredients(amountOfIngredients + 1)}
+            />
+            <GenericGreenButton
+              text="Remove Ingredient"
+              click={() => removeIngredientAndPopArray()}
+            />
+          </div>
+          <div className="mt-6">
+            <textarea
+              onChange={(e) => setContent(e.target.value)}
+              className="rounded-lg pt-2 pl-5 w-full h-40 px-2"
+              value={content}
+              placeholder="Recipe Instructions"
+            />
+          </div>
+        </div>
+        <GenericGreenButton
+          text="Update and save as Draft"
+          click={() => submitData(false)}
+        />
+        <GenericGreenButton text="Update Post" click={() => submitData(true)} />
+        <p>{fetchStatus}</p>
+        <p>{fetchError}</p>
+      </Layout>
+    </div>
+  );
+};
+
+export default EditPost;
