ab98e3063f90a9ec3b68daa84de0c53d19b39205
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-29 09:06:05 +0200

    Post new recipe API route

---
+++ pages/api/post/index.ts
@@ -0,0 +1,41 @@
+import { getSession } from "next-auth/react";
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
+
+// POST /api/post
+// Required fields in body: title
+// Optional fields in body: content
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+   const session = await getSession({ req });
+
+  if (session) {
+    const author = { connect: { email: session?.user?.email } };
+    const data = Object.assign(req.body, { author: author });
+    console.log(data);
+    try {
+      console.log("Trying...");
+      const result = await prisma.post.create({
+        data: data,
+      });
+      console.log("result");
+      console.log(result);
+      res.status(200).json(result);
+    } catch (error) {
+      res.status(500).send({
+        message: "Database Error",
+      });
+    }
+  }
+
+  if (!session) {
+    res.status(403).send({
+      message: "You need to be logged in to perform this action.",
+    });
+  }
+
+  // console.log("result");
+  // console.log(session);
+}
