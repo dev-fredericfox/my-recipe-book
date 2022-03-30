fa1e50e3d2785d3c294d0b99ac1195497806f4d0
Frederic Fox <dev.frederic.fox@gmail.com>
2022-03-30 20:35:17 +0200

    Elevate DeletePost fn, add visual confirmation

--- components/DashboardPosts.tsx
+++ components/DashboardPosts.tsx
@@ -1,28 +1,59 @@
+import { useState } from "react";
 import { Post } from "../lib/interfaces";
+import Link from "next/link";
+import PublishUnpublish from "../components/PublishUnpublish";
+import DeletePost from "../components/DeletePost";
+import { saveToDB } from "../lib/fetchHelper";
 
 interface Props {
-  post: Post[]
+  post: Post[];
 }
-  
-  export default function DashboardPosts({ post }: Props) { 
-    return (
-      <div>
-        {post.map( (el, key:number) => (
-                <div key={key} className="bg-slate-200 rounded-lg p-2 mt-3 flex flex-row">
-                <div className="bg-white w-12 text-4xl text-center pt-1 rounded-lg">
-                  <span>{el.category.categoryEmoji}</span>
-                </div>
-                <div className="grow px-2 py-3">
-                  <span className="font-bold text-lg">{el.title}</span>
-                </div>
-                <div className="py-3 px-2">
-                  <span className="text-center text-gray-500">Publish/Pause</span>
-                  <span className="text-center text-gray-500"> Delete</span>
-                </div>
-              </div>
-        ))}
-  
-      </div>
-    );
-  }
-  
+
+export default function DashboardPosts({ post }: Props) {
+  const [posts, setPosts] = useState(post);
+  const [fetchError, setFetchError] = useState("");
+  const [fetchResult, setFetchResult] = useState("");
+  const [fetchStatus, setFetchStatus] = useState("");
+  const [inProgressDeletionColor, setInProgressDeletionColor] = useState<number|null>()
+
+  const deletePost = async (id: number) => {
+    setFetchStatus("PROGRESS");
+    setInProgressDeletionColor(id)
+    try {
+      const result = await saveToDB("DELETE", id);
+      setFetchResult(result);
+      setFetchStatus("OK");
+      const cache = [...posts]
+      const postsMinusDeleted = cache.filter(el => el.id !== id)
+      setPosts(postsMinusDeleted)
+      setInProgressDeletionColor(null)
+    } catch(error) {
+      setFetchResult("Failed");
+      setFetchError(error as string)
+    }
+  };
+
+  return (
+    <div>
+      {posts.map((el, key: number) => (
+        <div
+          key={key}
+          className={`${inProgressDeletionColor === el.id ?'bg-red-400': 'bg-slate-200'} rounded-lg p-2 mt-3 flex flex-row`}
+        >
+          <div className="bg-white w-12 text-4xl text-center pt-1 rounded-lg">
+            <span>{el.category.categoryEmoji}</span>
+          </div>
+          <div className="grow px-2 py-3">
+            <span className="font-bold text-lg">
+              <Link href={`/edit/${el.id}`}>{el.title}</Link>
+            </span>
+          </div>
+          <div className="flex flex-row py-3 px-2">
+            <PublishUnpublish status={el.published} id={el.id} />
+            <DeletePost id={el.id} deletePost={deletePost} />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
