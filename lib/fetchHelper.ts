72f639137aa88d72931c7e780f3c857a1afeadf3
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-01 09:55:28 +0200

    move var outside of try block, add revalidate func

--- lib/fetchHelper.ts
+++ lib/fetchHelper.ts
@@ -1,19 +1,21 @@
 import { Post } from "../lib/interfaces";
+import { revalidateNow } from "./revalidateHelper";
 
 export async function saveToDB(method: string, id?: number, payload?: any) {
   console.log("clicked submitted");
+  let fetchUrl = "/api/";
+  let body = payload ?? "";
+
+  if (method === "POST") {
+    fetchUrl = `/api/post`;
+  }
+  if (method === "DELETE") {
+    fetchUrl = `/api/delete/${id}`;
+  }
+  if (method === "PUT") {
+    fetchUrl = `/api/update/${id}`;
+  }
   try {
-    let fetchUrl = "/api/";
-    let body = payload ?? "";
-    if (method === "POST") {
-      fetchUrl = `/api/post`;
-    }
-    if (method === "DELETE") {
-      fetchUrl = `/api/delete/${id}`;
-    }
-    if (method === "PUT") {
-      fetchUrl = `/api/update/${id}`;
-    }
     let response = await fetch(fetchUrl, {
       method: method,
       headers: { "Content-Type": "application/json" },
@@ -25,6 +27,7 @@ export async function saveToDB(method: string, id?: number, payload?: any) {
     } else if (response.status === 500) {
       throw Error("Database Error");
     } else if (response.status === 200) {
+      let revalidation = await revalidateNow()
       return result;
     } else {
       throw Error("Something went wrong");
