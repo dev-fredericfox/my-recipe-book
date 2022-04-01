9a62731d4f50fc0e5317cb15d1f42b02476b23d9
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-01 15:22:55 +0200

    Add authorizedToPublish handler for logged in users

--- pages/api/update/[id].ts
+++ pages/api/update/[id].ts
@@ -1,6 +1,8 @@
 import { getSession } from "next-auth/react";
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
+import { getUser } from "../../../lib/getUser";
+import { revalidateNow } from "../../../lib/revalidateHelper";
 
 // POST /api/post
 // Required fields in body: title
@@ -13,27 +15,37 @@ export default async function handle(
   const postId = parseInt(req.query.id as string);
 
   if (session) {
-    // Author has bug, but why?
-    // const author = { connect: { email: session?.user?.email } };
-    const data = Object.assign(req.body);
-    const connectCategory = { connect: { name: req.body.category } };
-    data.category ? data.category = connectCategory : null
-    console.log(data);
-    console.log("postId");
-    console.log(postId);
     try {
-      console.log("Trying...");
-      const result = await prisma.post.update({
-        where: { id: postId },
-        data: data,
-      });
-      console.log("result");
-      console.log(result);
-      res.status(200).json(result);
+      let user = await getUser(session?.user?.email);
+      if (!user[0].authorizedToPublish) {
+        throw 402;
+      } else {
+        const data = Object.assign(req.body);
+        const connectCategory = { connect: { name: req.body.category } };
+        data.category ? (data.category = connectCategory) : null;
+        console.log(data);
+        console.log("postId");
+        console.log(postId);
+        console.log("Trying...");
+        const result = await prisma.post.update({
+          where: { id: postId },
+          data: data,
+        });
+        console.log("result");
+        console.log(result);
+        let revalidation = await revalidateNow()
+        res.status(200).json(result);
+      }
     } catch (error) {
-      res.status(500).send({
-        message: "Database Error",
-      });
+      if (error === 402) {
+        res.status(403).send({
+          message: "You do not have the permission to perform this action.",
+        });
+      } else {
+        res.status(500).send({
+          message: "Database Error",
+        });
+      }
     }
   }
 
---
+++ prisma/migrations/20220401121741_add/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "authorizedToPublish" BOOLEAN NOT NULL DEFAULT false;
