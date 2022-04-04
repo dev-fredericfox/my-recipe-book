19a5eab876bf235924f5e63c9f48f2edcb5241e0
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-04 11:59:44 +0200

    Correct spelling

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,11 +15,11 @@ const About: NextPage = () => {
         <Title title="About me" />
         </div>
         <div className="mt-4">
-        <p>Hi, I am Fred and I am a JavaScript developer. I also like to cook and I like React!</p>
-        <p>So I made this page as part of my portfolio, but I also actually use it to store my recipes and access them quickly when I want to cook something.</p>
-        <p className="mt-2">This page was made with react and next.js. The Database is PostgreSQL.</p>
-        <p className="mt-0">Visit my full porfolio at: <a href="https://www.frederic-fox.com">www.frederic-fox.com</a></p>
-        <p className="mt-4">So long and thanks for all the fish!</p>
+        <p>Hi, I am Fred and I am a JavaScript developer. I also like React and cooking…</p>
+        <p>That&apos;s why I made this app as part of my portfolio (but I also use it to quickly access my recipes when I need to cook something)!</p>
+        <p className="mt-2">This page was made with React and next.js. The database is PostgreSQL.</p>
+        <p className="mt-0">Visit my complete porfolio at: <a href="https://www.frederic-fox.com">www.frederic-fox.com</a></p>
+        <p className="mt-4">So long and thanks for all the fish! ;)</p>
         <p>Fred</p>
         </div>
       </Layout>
