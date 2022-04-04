603a732c03a61760f8cd53e6d855221c9210713a
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-04 10:47:17 +0200

    Fix path

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,17 +4,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const path = req.body.pathToRevalidate
   // Check for secret to confirm this is a valid request
   if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    console.log({ message: "Invalid token" })
     return res.status(401).json({ message: "Invalid token" });
   }
 
   try {
-    await res.unstable_revalidate("/path-to-revalidate");
+    await res.unstable_revalidate(path);
     return res.json({ revalidated: true });
   } catch (err) {
     // If there was an error, Next.js will continue
     // to show the last successfully generated page
-    return res.status(500).send("Error revalidating");
+    return res.status(500).send(err);
   }
 }
