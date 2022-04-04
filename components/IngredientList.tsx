5ffaf44ad38c23758aa6cb065686a510f11be617
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-04 18:28:26 +0200

    Fix horizontal and vertical alignment

--- components/IngredientList.tsx
+++ components/IngredientList.tsx
@@ -19,13 +19,13 @@ export default function IngredientList({
       <div
       key={ingredient}
         onClick={() => setGathered(!gathered)}
-        className={`bg-slate-200 rounded-lg p-2 mt-3 flex flex-row border-2 border-slate-200 ${gathered ? 'border-green-600':''}`}
+        className={`bg-slate-200 rounded-lg p-2 mt-3 flex items-center flex-row border-2 border-slate-200 ${gathered ? 'border-green-600':''}`}
       >
         <div className="bg-white w-14 h-14 flex-none text-4xl text-center pt-1 rounded-lg">
           <span>{emoji}</span>
         </div>
-        <div className="px-2 py-3 flex-initial">
-          <span className="font-bold  text-lg">{ingredient}</span>
+        <div className="grow px-2 py-3 flex-initial">
+          <span className="font-bold text-lg">{ingredient}</span>
         </div>
         <div className="py-3 px-2 flex-none">
           <span className="text-center text-gray-500">
