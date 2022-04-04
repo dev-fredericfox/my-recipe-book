216258b354a11a3d46000d468452fa79e342c00c
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-04 10:42:31 +0200

    Make emoji a link

--- components/DashboardPosts.tsx
+++ components/DashboardPosts.tsx
@@ -54,8 +54,8 @@ export default function DashboardPosts({ post }: Props) {
             inProgressDeletionColor === el.id ? "bg-red-400" : "bg-slate-200"
           } rounded-lg p-2 mt-3 flex flex-row`}
         >
-          <div className="bg-white w-12 h-12 text-4xl text-center pt-1 rounded-lg">
-            <span>{el.category.categoryEmoji}</span>
+          <div className="bg-white w-12 h-12 cursor-pointer text-4xl text-center pt-1 rounded-lg">
+          <Link href={`/admin/edit/${el.id}`}>{el.category.categoryEmoji}</Link>
           </div>
           <div className="grow px-2 py-3">
             <span className="font-bold text-lg">
