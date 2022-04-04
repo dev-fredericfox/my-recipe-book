bbfca35828cabeaf72fa0c7a3a19098e1d32ca30
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-04 16:15:31 +0200

    Fix css for long ingredients

--- components/IngredientList.tsx
+++ components/IngredientList.tsx
@@ -21,13 +21,13 @@ export default function IngredientList({
         onClick={() => setGathered(!gathered)}
         className={`bg-slate-200 rounded-lg p-2 mt-3 flex flex-row border-2 border-slate-200 ${gathered ? 'border-green-600':''}`}
       >
-        <div className="bg-white w-12 text-4xl text-center pt-1 rounded-lg">
+        <div className="bg-white w-14 h-14 flex-none text-4xl text-center pt-1 rounded-lg">
           <span>{emoji}</span>
         </div>
-        <div className="grow px-2 py-3">
-          <span className="font-bold text-lg">{ingredient}</span>
+        <div className="px-2 py-3 flex-initial">
+          <span className="font-bold  text-lg">{ingredient}</span>
         </div>
-        <div className="py-3 px-2">
+        <div className="py-3 px-2 flex-none">
           <span className="text-center text-gray-500">
             {amount}
           </span>
