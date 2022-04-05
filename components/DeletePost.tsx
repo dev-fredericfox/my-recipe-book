ad0b7da38bea89811878ecdba0a8b90285b1d2b4
Frederic Fox <dev.frederic.fox@gmail.com>
2022-04-05 15:23:20 +0200

    Add Delete Modal

--- components/DeletePost.tsx
+++ components/DeletePost.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
+import DeletionConfirmationModal from "./DeleteConfirmationModal"
 interface Props {
   id: number;
-  deletePost: any
+  deletePost: any,
+  title: String,
 }
 
-export default function DeletePost({ id, deletePost }: Props) {
+export default function DeletePost({ id, deletePost, title }: Props) {
+  const [deletionModal, setDeletionModal] = useState(false);
+  const confirmDeletionAndCloseModal = () => {
+    setDeletionModal(false)
+    deletePost(id)
+  }
   return (
     <div>
-      <div onClick={() => {deletePost(id)}} className="cursor-pointer text-2xl ml-3 my-0 text-center text-red-500 font-extrabold">X</div>
+      {deletionModal && <DeletionConfirmationModal recipeName={title} confirmDeletionAndCloseModal={confirmDeletionAndCloseModal} cancelAndCloseModal={() => setDeletionModal(false)}/>}
+      <div onClick={() => {setDeletionModal(true)}} className="cursor-pointer text-2xl ml-3 my-0 text-center text-red-500 font-extrabold">X</div>
     </div>
   );
 }
